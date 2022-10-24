07f60fb5f30c26fc369cb0b54c7597abd27b2041
Mohamed Hassan <mohamed>
2022-10-24 21:37:30 +0200

    test record

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,6 @@ import { Component,ElementRef,ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-voice';
-  play=false
-  audio = new Audio('assets/all.ogg');
-  @ViewChild('test')
-  test!: ElementRef;
 
 
-
-
-
-  playAudio() {
-
-    if(this.play)this.audio.pause()
-    else this.audio.play()
-    this.play=!this.play
-}
-ngOnInit(): void {
-  this.audio.onended = ()=> {
-   this.play=false
- //  manageImageObjectsLevel();
-  }
-
-  this.test.nativeElement.addEventListener('ended', function() {
-  //  manageImageObjectsLevel();
-}).get(0).play();
-}
 }
